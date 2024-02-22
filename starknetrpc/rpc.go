e1fa78102e6390dce87970d68cc94bedec47eb6b
Lawliet-Chan <1576710154@qq.com>
2024-02-22 15:58:56 +0800

    jsonRPC Serve

--- starknetrpc/rpc.go
+++ starknetrpc/rpc.go
@@ -1,24 +1,125 @@
 package starknetrpc
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"github.com/NethermindEth/juno/core/felt"
 	"github.com/NethermindEth/juno/jsonrpc"
 	"github.com/NethermindEth/juno/rpc"
+	"github.com/NethermindEth/juno/utils"
+	"github.com/NethermindEth/juno/validator"
+	"github.com/rs/cors"
+	"github.com/sourcegraph/conc"
 	"github.com/yu-org/yu/common"
 	"github.com/yu-org/yu/core"
 	"github.com/yu-org/yu/core/kernel"
 	"itachi/cairo"
+	"net"
+	"net/http"
+	"runtime"
+	"time"
 )
 
 const CairoTripod = "cairo"
 
 type StarknetRPC struct {
 	chain *kernel.Kernel
+	log   utils.SimpleLogger
+	srv   *http.Server
+	//cfg   *StarknetRpcConfig
 }
 
-func NewStarknetRPC(chain *kernel.Kernel) *StarknetRPC {
-	return &StarknetRPC{chain: chain}
+type StarknetRpcConfig struct {
+	Host     string
+	Port     string
+	LogLevel int
+	LogColor bool
+}
+
+func NewStarknetRPC(chain *kernel.Kernel, cfg *StarknetRpcConfig) (*StarknetRPC, error) {
+	log, err := utils.NewZapLogger(utils.LogLevel(cfg.LogLevel), cfg.LogColor)
+	if err != nil {
+		return nil, err
+	}
+	s := &StarknetRPC{chain: chain, log: log}
+	maxGoroutines := 2 * runtime.GOMAXPROCS(0)
+	jsonrpcServer := jsonrpc.NewServer(maxGoroutines, s.log).WithValidator(validator.Validator())
+	methods, path := s.Methods()
+	err = jsonrpcServer.RegisterMethods(methods...)
+	if err != nil {
+		return nil, err
+	}
+	mux := http.NewServeMux()
+	httpHandler := jsonrpc.NewHTTP(jsonrpcServer, s.log)
+	mux.Handle(path, exactPathServer(path, httpHandler))
+
+	s.srv = &http.Server{
+		Addr:        net.JoinHostPort(cfg.Host, cfg.Port),
+		Handler:     cors.Default().Handler(mux),
+		ReadTimeout: 30 * time.Second,
+	}
+	return s, nil
+}
+
+func (s *StarknetRPC) Serve(ctx context.Context) error {
+	errCh := make(chan error)
+	defer close(errCh)
+
+	var wg conc.WaitGroup
+	defer wg.Wait()
+	wg.Go(func() {
+		if err := s.srv.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			errCh <- err
+		}
+	})
+
+	select {
+	case <-ctx.Done():
+		return s.srv.Shutdown(context.Background())
+	case err := <-errCh:
+		return err
+	}
+}
+
+func exactPathServer(path string, handler http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != path {
+			http.NotFound(w, r)
+			return
+		}
+		handler.ServeHTTP(w, r)
+	}
+}
+
+func (s *StarknetRPC) Methods() ([]jsonrpc.Method, string) {
+	return []jsonrpc.Method{
+		{
+			Name:    "starknet_addDeployAccountTransaction",
+			Params:  []jsonrpc.Parameter{{Name: "deploy_account_transaction"}},
+			Handler: s.AddTransaction,
+		},
+		{
+			Name:    "starknet_addDeclareTransaction",
+			Params:  []jsonrpc.Parameter{{Name: "declare_transaction"}},
+			Handler: s.AddTransaction,
+		},
+		{
+			Name:    "starknet_addInvokeTransaction",
+			Params:  []jsonrpc.Parameter{{Name: "invoke_transaction"}},
+			Handler: s.AddTransaction,
+		},
+		{
+			Name:    "starknet_call",
+			Params:  []jsonrpc.Parameter{{Name: "request"}, {Name: "block_id"}},
+			Handler: s.Call,
+		},
+		{
+			Name:    "starknet_getNonce",
+			Params:  []jsonrpc.Parameter{{Name: "block_id"}, {Name: "contract_address"}},
+			Handler: s.GetNonce,
+		},
+	}, "/v0_6"
 }
 
 func (s *StarknetRPC) AddTransaction(tx rpc.BroadcastedTransaction) (*rpc.AddTxResponse, *jsonrpc.Error) {
