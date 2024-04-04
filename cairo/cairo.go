a72995c9574210e9112d95b62fd47157cb8efc9c
xinran chen <lawliet@xinran-m1x.local>
2024-04-05 00:35:39 +0800

    execute state.PendingReadWriter

--- cairo/cairo.go
+++ cairo/cairo.go
@@ -102,34 +102,6 @@ func (c *Cairo) CheckTxn(txn *types.SignedTxn) error {
 	// Replace the txHash with the Hash of starknet Txn
 	txn.TxnHash = starkTx.Hash().Bytes()
 
-	//if txReq.Tx.Type == rpc.TxnDeclare && txReq.Tx.Version.Cmp(new(felt.Felt).SetUint64(2)) != -1 {
-	//	contractClass := make(map[string]any)
-	//	if err := json.Unmarshal(txReq.Tx.ContractClass, &contractClass); err != nil {
-	//		return fmt.Errorf("unmarshal contract class: %v", err)
-	//	}
-	//	sierraProg, ok := contractClass["sierra_program"]
-	//	if !ok {
-	//		return fmt.Errorf("{'sierra_program': ['Missing data for required field.']}")
-	//	}
-	//
-	//	sierraProgBytes, errIn := json.Marshal(sierraProg)
-	//	if errIn != nil {
-	//		return errIn
-	//	}
-	//
-	//	gwSierraProg, errIn := utils.Gzip64Encode(sierraProgBytes)
-	//	if errIn != nil {
-	//		return errIn
-	//	}
-	//
-	//	contractClass["sierra_program"] = gwSierraProg
-	//	newContractClass, err := json.Marshal(contractClass)
-	//	if err != nil {
-	//		return fmt.Errorf("marshal revised contract class: %v", err)
-	//	}
-	//	txReq.Tx.ContractClass = newContractClass
-	//}
-
 	newTxReqByt, err := json.Marshal(txReq)
 	if err != nil {
 		return err
