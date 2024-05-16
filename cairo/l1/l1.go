fe52e32a1d2dbb110866853f250cc7c0b8020c97
Lawliet-Chan <1576710154@qq.com>
2024-05-16 19:03:25 +0800

    add some

--- cairo/l1/l1.go
+++ cairo/l1/l1.go
@@ -1,10 +1,35 @@
 package l1
 
 import (
+	"context"
 	"github.com/NethermindEth/juno/core"
 	"github.com/NethermindEth/juno/core/felt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/yu-org/yu/core/kernel"
+	"itachi/cairo/config"
 	"itachi/cairo/l1/contract"
 )
+
+type L1 struct {
+	itachi *kernel.Kernel
+	ethL1  *EthSubscriber
+}
+
+func NewL1(itachi *kernel.Kernel, cfg *config.Config) (*L1, error) {
+	ethL1, err := NewEthSubscriber(cfg.EthClientAddress, common.HexToAddress(cfg.EthContractAddress))
+	if err != nil {
+		return nil, err
+	}
+	return &L1{
+		itachi: itachi,
+		ethL1:  ethL1,
+	}, nil
+}
+
+func (l *L1) Run(ctx context.Context) {
+	msgChan := make(chan *contract.StarknetLogMessageToL2)
+	l.ethL1.WatchLogMessageToL2(ctx, msgChan, nil, nil, nil)
+}
 
 func parseEventToL1Txn(event *contract.StarknetLogMessageToL2) *core.L1HandlerTransaction {
 	callData := make([]*felt.Felt, 0)
