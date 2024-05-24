fc8450edbc33834be27d118dc431239a2257adbb
Frost <froststarbook@gmail.com>
2024-05-24 18:54:21 +0800

    recover cairoChain

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/yu-org/yu/apps/poa"
 	"github.com/yu-org/yu/core/startup"
-	// "itachi/cairo/config"
+	"itachi/cairo/config"
 	"itachi/cmd/node/app"
 	"itachi/evm"
 )
@@ -11,8 +11,8 @@ import (
 func main() {
 	startup.InitDefaultKernelConfig()
 	poaCfg := poa.DefaultCfg(0)
-	// cairoCfg := config.LoadCairoCfg("./conf/cairo_cfg.toml")
+	cairoCfg := config.LoadCairoCfg("./conf/cairo_cfg.toml")
 
-	// app.StartUpChain(poaCfg, cairoCfg)
-	app.StartUpEvmChain(poaCfg,&evm.Config{})
+	app.StartUpChain(poaCfg, cairoCfg)
+	app.StartUpEvmChain(poaCfg, &evm.Config{})
 }
